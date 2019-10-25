1b48b819f9302f2e2f83c3f8c356bf6072643d67
Marcos <Marcos@eze1-lhp-b01910.synapse.com>
2019-10-25 19:40:39 -0300

    adding tostring methods

--- alquileres/models.py
+++ alquileres/models.py
@@ -2,14 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
 class City(models.Model):
     name = models.CharField(max_length=50)
     iata = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
 
 class Property(models.Model):
-    # todo serch for numeric constraints
     address = models.CharField(max_length=50)
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
@@ -17,6 +18,9 @@ class Property(models.Model):
     image = models.CharField(max_length=200)
     max_pax = models.IntegerField()
     city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
 class Host(User):
@@ -32,3 +36,6 @@ class RentalDate(models.Model):
     property = models.ForeignKey(Property, on_delete=models.CASCADE)
     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
     date = models.DateField()
+
+    def __str__(self):
+        return self.date
