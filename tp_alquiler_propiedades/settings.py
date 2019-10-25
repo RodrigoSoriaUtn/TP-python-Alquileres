4d56dc6bd2fe33688f175d940edb85bc0bbefe3a
Rodrigo Soria <rodrigo.soria98@hotmail.com>
2019-10-25 18:57:35 -0300

    app added to installed_Apps

--- tp_alquiler_propiedades/settings.py
+++ tp_alquiler_propiedades/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'alquileres.apps.AlquileresConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
