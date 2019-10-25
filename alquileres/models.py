e3430232407db32ef367018a9d907b160fdab3ee
Marcos <Marcos@eze1-lhp-b01910.synapse.com>
2019-10-25 18:58:48 -0300

    adding models and some git shit

--- alquileres/models.py
+++ alquileres/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    iata = models.CharField(max_length=10)
+
+
+class Property(models.Model):
+    # todo serch for numeric constraints
+    address = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    daily_price = models.DecimalField()
+    image = models.CharField(max_length=200)
+    max_pax = models.DecimalField()
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+
+class Host(models.User):
+    pass
+
+
+class Reservation(models.Model):
+    total_price = models.DecimalField()
+    created_date = models.DateTimeField()
+
+
+class RentalDate(models.Model):
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    date = models.DateField()
