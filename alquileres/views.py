f6ba6e133f8a1e45e00d4e9122015d518abc1970
Rodrigo Soria <rodrigo.soria98@hotmail.com>
2019-10-25 20:10:59 -0300

    WIP - listing properties

--- alquileres/views.py
+++ alquileres/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from django.template import loader
+from .models import Property
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the rental web page index.")
+    property_list = Property.objects[:5]
+    template = loader.get_template('alquileres/index.html')
+    context = {
+        'property_list' : property_list
+    }
+    return HttpResponse(template.render(context, request))
