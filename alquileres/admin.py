007ef75915ec38e194959e6852f96a6484f33bbd
Rodrigo Soria <rodrigo.soria98@hotmail.com>
2019-10-25 19:29:33 -0300

    Added models to admin page

--- alquileres/admin.py
+++ alquileres/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Reservation, City, Property, RentalDate
+
+admin.site.register({Reservation, City, Property, RentalDate})
