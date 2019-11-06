7f1357c2ff250418de8685b675db4ca1c3c33b20
Efactoring <DevOps@supervielle.com.ar>
2019-11-06 12:21:19 -0300

    views and routing WIP

--- alquileres/views.py
+++ alquileres/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render, HttpResponse
 from django.template import loader
 from .models import Property
@@ -11,3 +12,10 @@ def index(request):
         'property_list' : property_list
     }
     return HttpResponse(template.render(context, request))
+
+def viewProperty(request, property_id):
+    try:
+        property = Property.objects.get(pk=property_id)
+    except Property.DoesNotExist:
+        raise Http404("Property does not exist")
+    return render(request, 'alquileres/viewProperty.html', {'property': property})
--- tp_alquiler_propiedades/urls.py
+++ tp_alquiler_propiedades/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from alquileres import views
+
 urlpatterns = [
     path('rental', include('alquileres.urls')),
     path('admin/', admin.site.urls),
+    path('property/<int:property_id>/', views.viewProperty, name='viewProperty')
 ]
